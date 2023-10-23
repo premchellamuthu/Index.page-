da92870f855be0e18f968cd5ff8805e6c3757093
premchellamuthu <145644850+premchellamuthu@users.noreply.github.com>
2023-10-23 21:35:08 +0530

    API Fetch

--- script.js
+++ script.js
@@ -1,44 +1,77 @@
-let button=document.getElementById('add')
-let todolist=document.getElementById('todolist')
-let input=document.getElementById('input')
-//local storage,application
-let todos=[]
-window.onload=()=>{
-    todos = JSON.parse(localStorage.getItem('todos')) || []
-    todos.forEach(todo=>addtodo(todo))
-}
-button.addEventListener('click',()=>{
-    if(input.value===''){
-        alert('list cant be empty')
-    }
-    todos.push(input.value)
-    localStorage.setItem('todos',JSON.stringify(todos))
-    addtodo(input.value)
-    input.value=''
-    console.log(todos)
-})
+const searchBtn = document.getElementById('search-btn');
+const mealList = document.getElementById('meal');
+const mealDetailsContent = document.querySelector('.meal-details-content');
+const recipeCloseBtn = document.getElementById('recipe-close-btn');
 
-function addtodo(todo){
-     let para=document.createElement('p')
-     para.innerText=todo
-     todolist.appendChild(para)
-     
-     
-     para.addEventListener('click',()=>{
-        para.style.textDecoration='line-through'
-        remove(todo)
-     })
-    
-     para.addEventListener('dblclick',()=>{
-        todolist.removeChild(para)
-        remove(todo)
-     })
+// event listeners
+searchBtn.addEventListener('click', getMealList);
+mealList.addEventListener('click', getMealRecipe);
+recipeCloseBtn.addEventListener('click', () => {
+    mealDetailsContent.parentElement.classList.remove('showRecipe');
+});
 
+
+// get meal list that matches with the ingredients
+function getMealList(){
+    let searchInputTxt = document.getElementById('search-input').value.trim();
+    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    .then(response => response.json())
+    .then(data => {
+        let html = "";
+        if(data.meals){
+            data.meals.forEach(meal => {
+                html += `
+                    <div class = "meal-item" data-id = "${meal.idMeal}">
+                        <div class = "meal-img">
+                            <img src = "${meal.strMealThumb}" alt = "food">
+                        </div>
+                        <div class = "meal-name">
+                            <h3>${meal.strMeal}</h3>
+                            <a href = "#" class = "recipe-btn">Get Recipe</a>
+                        </div>
+                    </div>
+                `;
+            });
+            mealList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any meal!";
+            mealList.classList.add('notFound');
+        }
+
+        mealList.innerHTML = html;
+    });
 }
-function remove(todo){
-    let index=todos.indexOf(todo)
-    if(index>-1){
-        todos.splice(index,1)
+
+
+// get recipe of the meal
+function getMealRecipe(e){
+    e.preventDefault();
+    if(e.target.classList.contains('recipe-btn')){
+        let mealItem = e.target.parentElement.parentElement;
+        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+        .then(response => response.json())
+        .then(data => mealRecipeModal(data.meals));
     }
-    localStorage.setItem('todos',JSON.stringify(todos))
+}
+
+// create a modal
+function mealRecipeModal(meal){
+    console.log(meal);
+    meal = meal[0];
+    let html = `
+        <h2 class = "recipe-title">${meal.strMeal}</h2>
+        <p class = "recipe-category">${meal.strCategory}</p>
+        <div class = "recipe-instruct">
+            <h3>Instructions:</h3>
+            <p>${meal.strInstructions}</p>
+        </div>
+        <div class = "recipe-meal-img">
+            <img src = "${meal.strMealThumb}" alt = "">
+        </div>
+        <div class = "recipe-link">
+            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+        </div>
+    `;
+    mealDetailsContent.innerHTML = html;
+    mealDetailsContent.parentElement.classList.add('showRecipe');
 }
